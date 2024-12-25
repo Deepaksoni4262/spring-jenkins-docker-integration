ebfa0376e6b9a0212153754edee82513d6d2c885
Deepak Soni <deepaksoni@Deepaks-MacBook-Air.local>
2024-12-25 19:26:40 +0530

    first commit

---
+++ src/main/java/com/spring_jenkins_docker/com/Application.java
@@ -0,0 +1,21 @@
+package com.spring_jenkins_docker.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/greet")
+	public String greet(){
+		return "hello user";
+	}
+}
